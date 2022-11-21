757d465ef25ba992a71ac4075c7ea95f8f65f34d
Vlad Trifautan <vlad.trifautan@mail.ebs-integrator.com>
2022-11-21 10:04:52 +0200

    configured lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,16 @@
-export default {
-  "*.{ts, tsx}": ["eslint", "prettier --write"],
+// eslint-disable-next-line no-undef
+module.exports = {
+  // Check Typescript files
+  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+
+  // Lint and format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js|jsx)": (filenames) => [
+    // `yarn eslint --fix ${filenames.join(" ")}`,
+    ...filenames.map((f) => `yarn lint --fix ${f}`),
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) =>
+    `yarn prettier --write ${filenames.join(" ")}`,
 };
