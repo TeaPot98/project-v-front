ffe6dcb151a0d7c93c747f70f0dad6257ff6d675
Vlad Trifautan <vlad.trifautan@mail.ebs-integrator.com>
2022-11-21 12:59:19 +0200

    installed react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
