cd7a1fb59e0fbde590c57218095c77ac2f85a74c
TeaPot98 <trifautan@gmail.com>
2022-12-04 09:27:20 +0200

    created patient types popover

--- src/features/patients/pages/Patients.tsx
+++ src/features/patients/pages/Patients.tsx
@@ -1,13 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import { useNavigate } from "react-router-dom";
 
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 import { Table, TableColumn } from "components";
 import models from "models";
 import api from "api";
 
 export const Patients = () => {
-  const { data } = useQuery({ ...api.queries.patient.all() });
+  const navigate = useNavigate();
+
+  const { data: patients } = useQuery({ ...api.queries.patient.all() });
+  const { data: patientTypes } = useQuery({ ...api.queries.patientType.all() });
+
   const columns: TableColumn[] = [
     { id: "name", label: "Name", minWidth: 170 },
     {
@@ -22,7 +31,37 @@ export const Patients = () => {
 
   return (
     <Box sx={{ overflowY: "hidden", height: "100%" }}>
-      <Table columns={columns} rows={data} />
+      <PopupState variant="popover">
+        {(popupState) => (
+          <>
+            <Button
+              variant="contained"
+              sx={{ float: "right", mb: "1rem" }}
+              {...bindTrigger(popupState)}
+            >
+              New Patient
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+              {patientTypes?.map((patientType) => (
+                <MenuItem
+                  key={patientType.id}
+                  onClick={() => {
+                    popupState.close();
+                    navigate("/patients/create", {
+                      state: {
+                        patientTypeId: patientType.id,
+                      },
+                    });
+                  }}
+                >
+                  {patientType.name}
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
+        )}
+      </PopupState>
+      <Table columns={columns} rows={patients} />
     </Box>
   );
 };
