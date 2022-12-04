9f01beff27c6f9caa922b2a8dc5a60457fd18c18
TeaPot98 <trifautan@gmail.com>
2022-12-04 08:08:43 +0200

    created patients table

--- src/features/patients/pages/Patients.tsx
+++ src/features/patients/pages/Patients.tsx
@@ -1,3 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
+import Box from "@mui/material/Box";
+
+import { Table, TableColumn } from "components";
+import models from "models";
+import api from "api";
+
 export const Patients = () => {
-  return <div>Patients page</div>;
+  const { data } = useQuery({ ...api.queries.patient.all() });
+  const columns: TableColumn[] = [
+    { id: "name", label: "Name", minWidth: 170 },
+    {
+      id: "author",
+      label: "Author",
+      minWidth: 170,
+      align: "right",
+      format: (value: models.PatientType["author"]) =>
+        `${value.name} ${value.surname}`,
+    },
+  ];
+
+  return (
+    <Box sx={{ overflowY: "hidden", height: "100%" }}>
+      <Table columns={columns} rows={data} />
+    </Box>
+  );
 };
